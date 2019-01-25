4cd040f9baafd77a5672c407f639b5d2966437e7
Nikita <lerowhou@gmail.com>
2019-01-25 21:24:57 +0200

    added

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
   var next = document.getElementById('new-quote');
   var text = document.getElementById('text');
   var auth = document.getElementById('author');
-  var n = data.length-1;
 
-next.onclick = qu();
-function qu(){
-  var r = getRandomInt(data);
-  text.innetHTML = data[r].text;
-  auth.innetHTML = data[r].author;
-}
 
 function getRandomInt() {
-  return Math.floor(Math.random() * (n - 0)) + 0;
+  return Math.floor(Math.random() * (57 - 0)) + 0;
+}
+
+function quote(){
+  r = getRandomInt();
+  text.innerHTML = data[r].text;
+  auth.innerHTML = data[r].author;
+}
+window.onload=function(){
+  setInterval(quote,3000);
 }
